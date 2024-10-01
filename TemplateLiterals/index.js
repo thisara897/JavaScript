e10eb0700e79f7303b50ff24f9514db829c39e4d
thisara <warshangunasinghe345@gmail.com>
2024-10-01 20:12:06 +0530

    ADD second method to TemplateLiterals

--- TemplateLiterals/index.js
+++ TemplateLiterals/index.js
@@ -5,3 +5,12 @@ let total = 100;
 console.log(`Hello ${userName}`);
 console.log(`you have ${items} items in your cart`);
 console.log(`your total is $${total}`);
+
+
+// Second Method
+
+let text =  `Hello ${userName}
+    	     you have ${items} items in your cart
+             your total is $${total}`;
+
+console.log(text)
