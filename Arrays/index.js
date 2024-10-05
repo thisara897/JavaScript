f1930eea074abb46e69f2b7369a8b86332dab300
thisara <warshangunasinghe345@gmail.com>
2024-10-05 16:05:48 +0530

    array manipulation

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,2 +1,30 @@
 let fruits = ["orange","apple","banana"];
-console.log(fruits);
+
+
+// access each elements
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// change elements in the array
+
+fruits[0] = "coconut";
+
+// adding values
+
+fruits.push("lemon");
+fruits.pop(); // removes last element
+
+fruits.unshift("mango"); // add element to begining
+fruits.shift();//removes element from the begining
+
+let length = fruits.length;
+let index = fruits.indexOf("kiwi");
+
+console.log(index);
+console.log(length);
+
+
+
+console.log(fruits); 
